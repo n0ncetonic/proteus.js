4ebc1b9baa979a8573216b61d0b7e9aa39b3a9b3
Florian Keller <florian.keller@wire.com>
2017-03-28 15:01:55 +0200

    Update examples

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 const Proteus = require('./commonjs/proteus');
 
 const lastResort = Proteus.keys.PreKey.MAX_PREKEY_ID;
-const preKey = Proteus.keys.PreKey.new(lastResort);
+const preKey = new Proteus.keys.PreKey(lastResort);
 const serializedPreKey = preKey.serialise();
 
 console.log(serializedPreKey);
